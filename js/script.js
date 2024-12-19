2d4fe90a9614559284beb98cddcf28c14c094420
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-19 11:23:50 +0100

    feat: add js inner "card member"

--- js/script.js
+++ js/script.js
@@ -37,3 +37,27 @@ const teamMembers = [
   }
 ];
 
+//DOM ELEMENTS
+const teamContainer = document.querySelector('.container')
+const avatarImgElm = document.getElementById('avatar-img');
+const fullNameElm = document.getElementById('full-name');
+const roleElm = document.getElementById('role');
+const emailElm = document.getElementById('email');
+
+let cardMembers = ""
+for (let i = 0; i < teamMembers.length; i++) {
+  const {name, role, img, email} = teamMembers[i] /*SINTASSI ABBREVIATA CON L'USO DEL DESTRUCTURING CHE CREA UNA VARIABILE E GLI ASSEGNA AUTOMATICAMENTE IL VALORE DELL'OGGETTO AVENTE LA STESSA CHIAVE.*/
+  cardMembers += `
+    <div class="col d-flex justify-content-center mb-2 ">
+      <div class="avatar">
+        <img id="avatar-img" src=${img} alt="${name}">
+      </div>
+      <div class="d-flex flex-column justify-content-center bg-black text-white p-4" style="min-width: 300px;">
+        <h2 id="full-name" class="full-name fs-3">${name}</h2>
+        <h3 id="role" class="role fs-6">${role}</h3>
+        <p id="email" class="email fs-6 text-info">${email}</p>
+      </div>
+    </div>
+  `;
+}
+teamContainer.innerHTML = cardMembers;
